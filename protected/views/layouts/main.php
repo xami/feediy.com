33f351fec34f7ff8179621523c5490f957347eb2
= <=>
2011-08-31 22:20:42 +0800

    ss

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -29,9 +29,9 @@
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'首页', 'url'=>'/'),
-				array('label'=>'网站地图生成工具', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'联系', 'url'=>array('/site/contact')),
-                array('label'=>'其它站点网站地图缓存', 'url'=>'http://sitemap.feediy.com'),
+				array('label'=>'网站地图生成工具使用说明', 'url'=>array('/site/page', 'view'=>'about')),
+				array('label'=>'问题意见反馈', 'url'=>array('/site/contact')),
+                array('label'=>'网站地图结果缓存', 'url'=>'http://sitemap.feediy.com'),
 //				array('label'=>'登陆', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 //				array('label'=>'退出 ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
