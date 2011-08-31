373f30ea396b0334d787386750ac1463ce2cb426
= <=>
2011-08-31 21:41:42 +0800

    g

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -61,8 +61,18 @@
 
 </div><!-- page -->
 <script type="text/javascript">
-var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fed5c56b43a7085ccbf9c6b3d586d6421' type='text/javascript'%3E%3C/script%3E"));
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-25449639-1']);
+  _gaq.push(['_setDomainName', '.feediy.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
 </script>
 </body>
 </html>
